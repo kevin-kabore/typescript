3be13b5c7458a8d9e7788582c22f8b1c4c166b4e
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-01-30 12:50:26 -0800

    Adds basic webpack config for ts/react

---
+++ udemy-course/tsReact/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './src/index/tsx',
+  output: {
+    filename: './dist/bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.ts', '.tsx', '.js']
+  },
+  module: {
+    loaders: [{ test: /\.tsx?$/, loader: 'awesome-typescript-loader' }]
+  }
+};
